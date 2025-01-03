63367ca67505c48e259bf6a1ca5c9c8aaf7ff8f4
nephesh <jmmatthews@proton.me>
2025-01-03 15:22:10 -0600

    Add project var

--- flake.nix
+++ flake.nix
@@ -11,6 +11,7 @@
 
   outputs = { self, nixpkgs, bootdev }@inputs:
     let
+      project = "bookbot";
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
       forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
         pkgs = import nixpkgs { inherit system; };
@@ -20,15 +21,15 @@
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
           venvDir = ".venv";
-          packages = with pkgs; [ python311 go gotools ] ++
+          packages = with pkgs; [ python314 go gotools ] ++
             (with pkgs.python311Packages; [
               pip
               venvShellHook
             ]);
 
-          shellHook = ''export GOBIN=$HOME/Projects/bookbot/.go/bin
-                        export GOPATH=$HOME/Projects/bookbot/.go/bin
-                        export PATH=$PATH:$HOME/Projects/bookbot/.go/bin
+          shellHook = ''export GOBIN=$HOME/Projects/${project}/.go/bin
+                        export GOPATH=$HOME/Projects/${project}/.go/bin
+                        export PATH=$PATH:$HOME/Projects/${project}/.go/bin
                         go telemetry off'';
         };
       });
