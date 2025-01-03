b191c4f96d5d2b8f9e9cc5ed3ecf5a33cdde3dfe
nephesh <jmmatthews@proton.me>
2025-01-03 14:53:51 -0600

    Remove countLetters

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ def countWords(corpus):
     wordCount = corpus.split(" ")
     return wordCount
 
-def countLetters(list1, list2):
-    for char in list1:
-        for word in list2:
-            result = word.count(char)
-
 def countChars(corpus):
     charList = list(set([char.lower() for char in corpus if char.lower().isalpha()]))
     countDict = {char: count for char,count in zip(charList, list(map(lambda char: corpus.count(char), charList)))}
