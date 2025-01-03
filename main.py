22d2de26ba1bb14b90754043f630298712744e41
nephesh <jmmatthews@proton.me>
2025-01-03 14:47:37 -0600

    Add dict comp

--- main.py
+++ main.py
@@ -16,13 +16,13 @@ def countChars(corpus):
     charList = list(set([char.lower() for char in corpus if char.lower().isalpha()]))
     wordList = countWords(corpus)
     #countList = [sum(list(map(lambda char: word.count(char), charList))) for word in wordList]
-    test = list(map(lambda char: corpus.count(char), charList))
+    test = {char: count for char,count in zip(charList, list(map(lambda char: corpus.count(char), charList)))}
     print(test)
 
 def report(corpus):
     print(f"--- Begin report of frankenstein.txt ---")
     print(f"{len(countWords(corpus))} words found in document\n\n")
-    for letter in countChars(corpus):
+    for letter,count in countChars(corpus):
         print(f'The "{letter}" character was found {count} times')
 
 #countWords(main())
